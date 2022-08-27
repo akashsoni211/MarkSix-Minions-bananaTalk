21306fc95d27208035bb7f073122a9e4b6fc6974
Akash <42184758+akashsoni211@users.noreply.github.com>
2022-08-27 11:19:12 +0530

    last commit
    Project is ready to use

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 var textInput = document.querySelector("#textinput");
 var textTranslate = document.querySelector("#btntranslate");
 var textOutput = document.querySelector("#textoutput");
-
 textTranslate.addEventListener("click", translateText);
+
+function createUrl(){
+    return "https://api.funtranslations.com/translate/minion.json"+"?"+"text="+textInput.value;
+}
+
+function errorHandler(error){
+    return console.log("Error Occured" + error)
+}
+
+function outputText(text){
+    return textOutput.innerHTML = text;
+}
+
+function fetchurl(url){
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>json.contents.translated)
+    .then(outputText)
+    .catch(errorHandler)
+}
+
 function translateText(){
-    textOutput.innerHTML = textInput.value;
+    var output = fetchurl(createUrl());
+    textOutput.innerHTML = output;
 }
